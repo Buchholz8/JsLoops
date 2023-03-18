83d62138ef2dcf0eaf3e46d06b509fa961b68455
markb <markbuchholz8@gmail.com>
2023-03-18 10:48:09 -0600

    loops

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let counter = 0;
+while(counter <= 50) {
+console.log(counter);
+counter = counter + 1;
+}
+let neg_counter = 0;
+while(neg_counter >= -50) {
+console.log(neg_counter);
+neg_counter = neg_counter - 1;
+}
+let pos_counter = 0;
+while(pos_counter <= 100) {
+ console.log(pos_counter);
+pos_counter = pos_counter + 2;
+}
