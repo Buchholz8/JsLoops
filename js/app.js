29b164c0fc5854ad85e931524a9493cb85c3b9ff
markb <markbuchholz8@gmail.com>
2023-03-18 14:26:27 -0600

    loops

--- js/app.js
+++ js/app.js
@@ -13,3 +13,22 @@ while(pos_counter <= 100) {
  console.log(pos_counter);
 pos_counter = pos_counter + 2;
 }
+let players = [`janice`,`jake`,`jack`];
+let user_counter = 0;
+let user_points = [20,40,80];
+let user_injury = [`true`,`false`,`false`];
+for(let user_counter=0; user_counter<players.length; user_counter=user_counter+1){
+
+    console.log(user_counter)
+}
+for(let user_counter=0; user_counter<user_points.length; user_counter=user_counter+1){
+
+    console.log(`${players[user_counter]} has scored ${user_points[user_counter]} points`)
+}
+for(let user_counter=0; user_counter<players.length; user_counter=user_counter+1){
+
+    if(user_injury[user_counter] === false){
+       console.log(`${players[user_counter]} has scored ${user_points[user_counter]} points`) 
+    }
+user_counter = user_counter +1;
+}
